3dc4f6c3bf4f34e39e64d01df5001e6d186a0138
adityamishra28203 <adityamishraubi@gmail.com>
2024-10-09 23:41:39 +0530

    config file removed

--- week-3/03-mongo/config.js
+++
@@ -1,4 +0,0 @@
-
-modeule.exports = {
-    MongoDB_URL: "mongodb+srv://adityamishraubi:9460%40Db@cluster0.2rqwy.mongodb.net/courseApp"
-}
--- week-3/04-mongo-with-jwt-auth/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    JWT_SECRET: "aditya_server_key",
-    MongoDB_URL: 'mongodb+srv://adityamishraubi:9460%40Db@cluster0.2rqwy.mongodb.net/courseApp_JWT'
-}
